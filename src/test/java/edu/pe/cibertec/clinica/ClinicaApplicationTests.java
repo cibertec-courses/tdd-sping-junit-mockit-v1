17aa7428d7078ce12f762ecae697a8f4e3333885
Christiam Mendoza <ch.alb.rz@hotmail.com>
2026-03-26 17:06:41 -0500

    Initial Commit

---
+++ src/test/java/edu/pe/cibertec/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
